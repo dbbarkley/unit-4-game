3fcb2d5ba67cae4ce9dc4c3d0c502886d86b4b0d
Daniel Barkley <danielbarkley@daniels-mbp.attlocal.net>
2019-01-07 06:51:12 -0600

    added html and css, started on js.

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+
+
+var scoreToGuess = Math.floor(Math.random() * 120) + 19;
+
+$("#random_number").text(scoreToGuess);
+
+var 
+
+
+
+
+
+$(".red-crystal").on("click", function() {
+    alert("you clicked the red crystal");
+});
