7874e0707afd49c34eb72299de83e6527f0e9b58
Daniel Barkley <danielbarkley@Daniels-MacBook-Pro.local>
2019-01-08 12:43:52 -0600

    added JS. Need to create function to reset the game once a user wins/losses. After this is done I believe It will be done.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,132 @@
 
 
-var scoreToGuess = Math.floor(Math.random() * 120) + 19;
+function gameFunction() {
+    $("#user_score").text(counter);
+    var scoreToGuess = Math.floor(Math.random() * 102) + 19;
+    $("#random_number").text(scoreToGuess);
 
-$("#random_number").text(scoreToGuess);
+    $(counter).val(" ");
+    console.log(counter);
 
-var 
+    var wins = 0;
+
+    var losses = 0;
+
+    var counter = 0;
+
+    var redCrystalValue = Math.floor(Math.random() * 12) +1;
+
+    var blueCrystalValue = Math.floor(Math.random() * 12) +1;
+
+    var greenCrystalValue = Math.floor(Math.random() * 12) +1;
+
+    var orangeCrystalValue = Math.floor(Math.random() * 12) +1;
+
+
+    $(".red-crystal").on("click", function() {
+        counter += redCrystalValue;
+        $("#user_score").text(counter);  
+
+        if (counter == scoreToGuess) {
+            wins += 1;
+            $("#wins").text(wins);
+            alert("You Win!");
+                         
+            $("#user_score").text("0");
+
+            gameFunction();
+        }
+    
+        else if (counter >= scoreToGuess) {
+            losses += 1;
+            $("#losses").text(losses);
+            alert("You Lose!"); 
+    
+            $("#user_score").text("0");
+
+            gameFunction();
+        }
+    });
+    
+    $(".blue-crystal").on("click", function() {
+        counter += blueCrystalValue;
+        $("#user_score").text(counter);  
+
+        if (counter === scoreToGuess) {
+            wins += 1;
+            $("#wins").text(wins);
+            alert("You Win!");
+
+
+            $("#user_score").text("0");
+
+            gameFunction();
+        }
+    
+        else if (counter >= scoreToGuess) {
+            losses += 1;
+            $("#losses").text(losses);
+            alert("You Lose!");
+
+
+            $("#user_score").text("0");
+
+            gameFunction();
+        }
+    });
+
+    $(".green-crystal").on("click", function() {
+        counter += greenCrystalValue;
+        $("#user_score").text(counter);  
+        
+        if (counter === scoreToGuess) {
+            wins += 1;
+            $("#wins").text(wins);
+            alert("You Win!");
+
+            $("#user_score").text("0");
+
+            gameFunction();
+        }
+    
+        else if (counter >= scoreToGuess) {
+            losses += 1;
+            $("#losses").text(losses);
+            alert("You Lose!");
+
+            $("#user_score").text("0");
+
+            gameFunction();
+        }
+    });
+
+    $(".orange-crystal").on("click", function() {
+        counter += orangeCrystalValue;
+        $("#user_score").text(counter);  
+
+        if (counter === scoreToGuess) {
+            wins += 1;
+            $("#wins").text(wins);
+            alert("You Win!");
+
+            $("#user_score").text("0");
+
+            gameFunction();
+        }
+    
+        else if (counter >= scoreToGuess) {
+            losses += 1;
+            $("#losses").text(losses);
+            alert("You Lose!");
+
+            $("#user_score").text("0");
+
+            gameFunction();
+        }
+    });
+
+}
 
 
 
-
-
-$(".red-crystal").on("click", function() {
-    alert("you clicked the red crystal");
-});
+window.onload = gameFunction;
